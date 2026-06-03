30378ff8fdb9d80bf7e728d3517159c49f19663f
Patrick Arndt <patrick.arndt@inevvo-solutions.com>
2026-06-03 12:37:37 +0200

    feat: add favicon.ico and update layout to use it as the site icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Uprox</title>
+</svelte:head>
 <ModeWatcher />
 {@render children()}
